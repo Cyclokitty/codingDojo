8b577c89391fcf21d11df3128deca1e12f9e459d
Laura Vingada <laura@vingada.com>
2017-05-19 19:27:02 -0400

    started chapter 2 algos

---
+++ chapter2.js
@@ -0,0 +1,41 @@
+function sigma(num) {
+  var total = 0;
+  for (var i = 0; i <= num; i++) {
+    total += i;
+  }
+  return total;
+}
+
+sigma(5);
+
+function factorial(num) {
+  var total = 1;
+  for (var i = 1; i <= num; i++) {
+    total *= i;
+  }
+  return total;
+}
+
+factorial(5);
+
+function drawLeftStars(num) {
+  console.log('*'.repeat(num));
+}
+
+drawLeftStars(20);
+
+function drawRightStars(num) {
+  var standard = 75;
+  console.log(" ".repeat(standard - num) + "*".repeat(num));
+}
+
+drawRightStars(36);
+
+function drawCenteredStars(num) {
+  var standard = 75;
+  var leftHand = 75 / 2;
+  var rightHand = 75 / 2;
+  console.log(' '.repeat(leftHand - (num / 2)) + '*'.repeat(num) + ' '.repeat(rightHand - (num / 2)));
+}
+
+drawCenteredStars(15)
