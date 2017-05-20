c7fbf1df318cf793b87478e33fd46c84b0412c7a
Laura Vingada <laura@vingada.com>
2017-05-20 15:13:58 -0400

    more algos

--- chapter2.js
+++ chapter2.js
@@ -61,3 +61,82 @@ function drawCenteredChar(num, char) {
 }
 
 drawCenteredChar(15, '@')
+
+function threesFives() {
+  var total = 0;
+  for (var i = 0; i <= 5000; i++) {
+    if ((i % 3 === 0 && i % 5 !== 0) || (i % 3 !== 0 && 5 % 0 === 0)) {
+      total += i;
+      console.log(i)
+    }
+  }
+  return total;
+}
+
+threesFives();
+
+function betterThreesFives(start, end) {
+  var total = 0;
+  for (var i = start; i <= end; i++) {
+    if ((i % 3 === 0 && i % 5 !== 0) || (i % 3 !== 0 && 5 % 0 === 0)) {
+      total += i;
+      console.log(i)
+    }
+  }
+  return total;
+}
+
+betterThreesFives(100, 900);
+
+function generateCoinChange(cents) {
+  var loonies = 0;
+  var halfDollars = 0;
+  var quarters = 0;
+  var nickels = 0;
+  var dimes = 0;
+  var pennies = 0;
+  while (cents > 0) {
+    if (cents >= 100) {
+      loonies++;
+      cents -= 100;
+    } else if (cents >= 50) {
+      halfDollars++;
+      cents -= 50;
+    } else if (cents >= 25) {
+      quarters++;
+      cents -= 25;
+    } else if (cents >= 10) {
+      dimes++;
+      cents -= 10;
+    } else if (cents >= 5) {
+      nickels++;
+      cents -= 5;
+    } else if (cents >= 1) {
+      pennies++;
+      cents -= 1;
+    }
+  }
+  return `loonies: ${loonies}, halfDollars: ${halfDollars}, quarters: ${quarters}, dimes: ${dimes}, nickels: ${nickels}, pennies: ${pennies}`;
+}
+
+generateCoinChange(553);
+
+function messyMath(num) {
+  var sum = 0;
+  for (var i = 0; i <= num; i++) {
+    if (i % 3 === 0) {
+      continue;
+    } else if (i % 7 === 0) {
+      sum += (i + i);
+    } else if (num / 3 === i) {
+      sum = -1;
+      break;
+
+    } else {
+      sum += i;
+    }
+  }
+  return sum;
+}
+
+messyMath(15);
